1cb325d595074b6c5569e4726ea849f8354b18f5
Alexandre Luis Siqueira <alexandre1760@hotmail.com>
2021-04-14 08:00:07 -0300

    Validando o cadastro de categorias

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,6 +8,11 @@ const cateforiesRepository = new CategoriesRepository()
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlreadyExists = cateforiesRepository.findByName(name)
+
+  if(categoryAlreadyExists){
+    return response.status(400).json({ error: "Category Already exists!"})
+  }
   cateforiesRepository.create({ name, description})
 
 
