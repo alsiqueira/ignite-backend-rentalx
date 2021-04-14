d981aa2f375620112f16fd59dcceee9ebba5d935
Alexandre Luis Siqueira <alexandre1760@hotmail.com>
2021-04-14 09:40:23 -0300

    Utilizando o Principio de Responsabilidade Unica(SRP)

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,26 +1,22 @@
 import { response, Router } from "express";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
-const cateforiesRepository = new CategoriesRepository()
+const categoriesRepository = new CategoriesRepository()
 
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const categoryAlreadyExists = cateforiesRepository.findByName(name)
+  const createCategoryService = new CreateCategoryService(categoriesRepository)
 
-  if(categoryAlreadyExists){
-    return response.status(400).json({ error: "Category Already exists!"})
-  }
-  cateforiesRepository.create({ name, description})
-
-
+  createCategoryService.execute({name, description})
   return response.status(201).send()
 })
 
 categoriesRoutes.get("/", (request, response) => {
-  const all = cateforiesRepository.list()
+  const all = categoriesRepository.list()
   return response.json(all)
 })
 export { categoriesRoutes };
