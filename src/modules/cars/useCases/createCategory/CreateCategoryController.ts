7ddc312f638c2b7eae3a1290a881dab02877c5bc
Alexandre Luis Siqueira <alexandre1760@hotmail.com>
2021-04-16 16:10:58 -0300

    Instalacndo o TSyringe e refatorando a injeçao de dependencia do createCategory

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -1,13 +1,14 @@
 import { Response, Request } from 'express'
 import { CreateCategoryUseCase } from './CreateCategoryUseCase';
+import { container } from 'tsyringe'
 
 class CreateCategoryController {
-	constructor(private createCategoryUseCase: CreateCategoryUseCase){}
 
    async hendle(request: Request, response: Response): Promise<Response> {
 			const { name, description } = request.body;
+			const createCategoryUseCase = container.resolve(CreateCategoryUseCase)
 
-		await	this.createCategoryUseCase.execute({name, description})
+		await	createCategoryUseCase.execute({name, description})
 
 			return response.status(201).send()
 		}
