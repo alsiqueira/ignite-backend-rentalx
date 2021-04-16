fbf65c3ef5834df0ecc289cb299167e7bc9102ab
Alexandre Luis Siqueira <alexandre1760@hotmail.com>
2021-04-16 11:54:37 -0300

    Refaturando o model de categoria

--- src/Modules/cars/entities/Category.ts
+++ src/Modules/cars/entities/Category.ts
@@ -1,8 +1,19 @@
 import { v4 as uuidv4 } from "uuid";
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
+
+@Entity("categories")
 class Category {
+
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name: String;
+
+  @Column()
   description: String;
+
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
